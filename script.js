a17662eba70a54d829f532fd339eceb0188cf27d
OneZ3ro <emilioangelo665@gmail.com>
2023-07-13 15:31:59 +0200

    il bottone mi da numeri casuali della tombola

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const tabella = document.getElementById("tabella");
 
-const addCell_and_btn = () => {
+const estrazione_num = (event) => {
+  const ul = document.getElementById("lista-lucky-num");
+  const li_num_random = document.createElement("li");
+  const random_num = Math.floor(Math.random() * numeri_tombola.length) + 1;
+
+  li_num_random.innerText = `Numero fortunato: ${random_num}`;
+
+  console.log(random_num);
+  console.log(event.target);
+
+  ul.appendChild(li_num_random);
+};
+
+const numeri_tombola = [];
+
+const add_celle_btn_numeriTombola = () => {
   const bottone = document.createElement("button");
   bottone.innerText = "Tenta la fortuna!";
   for (let i = 0; i < 76; i++) {
     const cella = document.createElement("div");
-    cella.innerText = i + 1;
+    numeri_tombola[i] = i + 1;
+
+    cella.innerText = numeri_tombola[i];
     cella.style.display = "inline-block";
     cella.className = "cella";
     tabella.appendChild(cella);
   }
 
+  bottone.className = "bottone";
+  bottone.onclick = estrazione_num;
   tabella.appendChild(bottone);
 };
-addCell_and_btn();
+add_celle_btn_numeriTombola();
